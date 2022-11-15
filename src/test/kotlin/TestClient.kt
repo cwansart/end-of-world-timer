dc934c563d1fd06356ff5ad3fc1de4546ae1484a
Christian Wansart <cwansart@users.noreply.github.com>
2022-11-15 15:01:15 +0100

    fix test

--- src/test/kotlin/TestClient.kt
+++ src/test/kotlin/TestClient.kt
@@ -1,12 +1,18 @@
 import kotlinx.browser.document
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.assertContains
 
 class TestClient {
     @Test
     fun testSayHello() {
         val container = document.createElement("div")
-        container.sayHello()
-        assertEquals("Hello from JS", container.textContent)
+        container.addContents()
+        container.textContent?.let {
+            assertContains(it, "Seconds remaining")
+            assertContains(it, "Minutes remaining")
+            assertContains(it, "Hours remaining")
+            assertContains(it, "Days remaining")
+            assertContains(it, "Weeks remaining")
+        }
     }
 } 
